cb2d4194883bbaee690616c62401b6f777c0fb55
ARUN KUMAR N <148625880+arunkumar-1122002@users.noreply.github.com>
2023-12-09 20:44:12 +0530

    Update _15puzzle.java

--- _15puzzle.java
+++ _15puzzle.java
@@ -1,49 +1,83 @@
-
 package _15puzzle;
 
+import java.util.Collections;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Stack;
 
-public class _15puzzle {
- static Stack<String> stack=new Stack<>();  
-static int[][] board=new int[4][4];
-static int boardsize=board.length;
+public class _15puzzle 
+{
+ static Stack<String> stack=new Stack<>(); 
+ static Stack<String> solution=new Stack<>();
+static int[][] board;
+static int[][] reset;
+static int[][] resetempty=new int[1][2];
+static int[][] empty=new int[1][2];
+static int boardsize;
     public static void main(String[] args) {
         Scanner inp=new Scanner(System.in);
+        stack.push("");
+        System.out.println("Enter board size:");
+        boardsize=inp.nextInt();
+        board=new int[boardsize][boardsize];
+        reset=new int[boardsize][boardsize];
         createboard();
         display();
         System.out.println("");
         shuffleboard();
+        updatereset();
         display();
-        int[][] empty=find_empty();
+        //empty=find_empty();
         while(check_answer())
         {
-            System.out.println("1.left\n2.right\n3.top\n4.down\n5.hint");
-            int n=inp.nextInt();
+            System.out.println("1.left\n2.right\n3.top\n4.down\n5.reset and give solution\n6.Give hint");
+            int n=inp.nextInt(); 
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n");
             switch(n)
             {
                 case 1:
-                    empty=moveleft(empty);
+                    empty=moveleft();
                     display();
                     break;
                 case 2:
-                    empty=moveright(empty);
+                    empty=moveright();
                     display();
                     break;
                 case 3:
-                    empty=movetop(empty);
+                    empty=movetop();
                     display();
                     break;
                 case 4:
-                    empty=movedown(empty);
+                    empty=movedown();
                     display();
                     break;
                 case 5:
-                    System.out.println("Go "+stack.peek());
+                    boardreset(); 
+                    System.out.println("Board reset");
                     break;
+                case 6:
+                    System.out.println("Go "+stack.peek()+"\n");
+                    break;
+                default:
+                    System.out.println("Select correct option in above");
+                    
             }
         }
+    }
+    static void boardreset()
+    {
+               empty[0][0]=resetempty[0][0];
+               empty[0][1]=resetempty[0][1];
+                  for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+               board[i][j]=reset[i][j];          
+            }           
+        }
+                    display();
+               Collections.reverse(solution);
+               System.out.println("Do this move to win game :\n " + solution);
+               Collections.reverse(solution);
+               stack=solution;
     }
     static boolean check_answer()
     {
@@ -57,7 +91,7 @@ static int boardsize=board.length;
                     return true;
                 valuegenerator++;
                 
-                if(valuegenerator==16)
+                if(valuegenerator==boardsize*boardsize)
                 {
                     System.out.println("CONGRATES YOU WON THE GAME");
                     return false;
@@ -73,67 +107,61 @@ static int boardsize=board.length;
             for (int j = 0; j < boardsize; j++) {
              board[i][j]=valuegenerator;
                 valuegenerator++;
-            }
-            
+            }      
         }
-        board[3][3]=0;
+        board[boardsize-1][boardsize-1]=0;
+      empty[0][0]=boardsize-1;
+      empty[0][1]=boardsize-1;
     }
     static void shuffleboard()
     {
-        int[][] empty={{3,3}};
         Random rd=new Random();
-        for (int i = 0; i <15; i++) {
+        for (int i = 0; i <boardsize*3; i++) {
             int move=rd.nextInt(4);
             switch(move%4)
             {
-                case 0:
+                case 0://shuffle top if its impossible move down
                     if(empty[0][0]==0)
                     {
-                        empty=movedown(empty);
-                        stack.push("top");
+                        empty=movedown();
                     }
                     else{
-                    empty=movetop(empty);
-                    stack.push("down");
+                    empty=movetop();
                     }
                     break;
-                case 1:
-                    if(empty[0][0]==3)
+                case 1://shuffle down if its impossible move top
+                    if(empty[0][0]==boardsize-1)
                     {
-                        empty=movetop(empty);
-                        stack.push("down");
+                        empty=movetop();
                     }
                     else
                     {
-                    empty=movedown(empty);
-                    stack.push("top");
+                    empty=movedown();
                     }
                     break;
-                case 2:
+                case 2://shuffle left if its impossible move right
                     if(empty[0][1]==0)
                     {
-                        empty=moveright(empty);
-                        stack.push("left");
+                        empty=moveright();
                     }
                     else
                     {
-                    empty=moveleft(empty);
-                    stack.push("right");
+                    empty=moveleft();
                     }
                     break;
-                case 3:
-                    if(empty[0][1]==3)
+                case 3://shuffle right if its impossible move left
+                    if(empty[0][1]==boardsize-1)
                     {
-                        empty=moveleft(empty);
-                        stack.push("right");
+                        empty=moveleft();
                     }
                     else
                     {
-                    empty=moveright(empty);
-                    stack.push("left");
-                    }break;
+                    empty=moveright();
+                    }
+                    break;
             }
         }
+        solution=stack;
     }
     static void display()
     {
@@ -156,7 +184,7 @@ static int boardsize=board.length;
             
         }
     }
-    static int[][] movetop(int[][] empty)
+    static int[][] movetop()
     {
         int row=empty[0][0];
         int col=empty[0][1];
@@ -166,16 +194,17 @@ static int boardsize=board.length;
         }
         else
         {
+            if(stack.peek()=="top")
+                stack.pop();
+            else
             stack.push("down");
-         int temp=board[row][col];
-         board[row][col]=board[row-1][col];
-         board[row-1][col]=temp;
+            swap(row,col,row-1,col);
          return new int[][]{{row-1,col}};
          
         }
         return empty;
     }
-    static int[][] moveleft(int[][] empty)
+    static int[][] moveleft()
     {
         int row=empty[0][0];
         int col=empty[0][1];
@@ -185,62 +214,69 @@ static int boardsize=board.length;
         }
         else
         {
+            if(stack.peek()=="left")
+                stack.pop();
+            else
             stack.push("right");
-         int temp=board[row][col];
-         board[row][col]=board[row][col-1];
-         board[row][col-1]=temp;
+            swap(row,col,row,col-1);
          return new int[][]{{row,col-1}};
         }
         
         return empty;
     }
-    static int[][] moveright(int[][] empty)
+    static int[][] moveright()
     {
         int row=empty[0][0];
         int col=empty[0][1];
-        if(col==3)
+        if(col==boardsize-1)
         {
             System.out.println("You cannot move right...invalid move");
         }
         else
         {
+            if(stack.peek()=="right")
+                stack.pop();
+            else
             stack.push("left");
-         int temp=board[row][col];
-         board[row][col]=board[row][col+1];
-         board[row][col+1]=temp;
+            swap(row,col,row,col+1);
          return new int[][]{{row,col+1}};
         }
        return empty; 
         
     }
-    static int[][] movedown(int[][] empty)
+    static int[][] movedown()
     {
         int row=empty[0][0];
         int col=empty[0][1];
-        if(row==3)
+        if(row==boardsize-1)
         {
             System.out.println("You cannot move down...invalid move");
         }
         else
         {
+            if(stack.peek()=="down")
+                stack.pop();
+            else
             stack.push("top");
-         int temp=board[row][col];
-         board[row][col]=board[row+1][col];
-         board[row+1][col]=temp;
+            swap(row,col,row+1,col);
          return new int[][]{{row+1,col}};
         }
      return empty;   
     }
-    static int[][] find_empty()
+    static void updatereset()
     {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board.length; j++) {
-                if(board[i][j]==0)
-                    return new int[][]{{i,j}};
-                
-            }
-            
+               reset[i][j]=board[i][j];          
+            }           
         }
-        return new int[0][0];//This return never work...because our board always have 0
+               resetempty[0][0]=empty[0][0];
+               resetempty[0][1]=empty[0][1];
+    }
+    static void swap(int source_row,int source_col,int dest_row,int dest_col)
+    {
+        int temp=board[source_row][source_col];
+         board[source_row][source_col]=board[dest_row][dest_col];
+         board[dest_row][dest_col]=temp;
     }
 }
